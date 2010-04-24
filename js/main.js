eef6af1783925e4d7af810a10cb24856ee9ca9ec
Atul Varma <avarma@mozilla.com>
2010-04-24 13:33:41 -0700

    added login form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
 $(window).ready(
   function() {
-    window.addEventListener(
-      "hashchange",
+    const BASE_TITLE = document.title;
+
+    function onLoginChange() {
+      var username = $("#login-username").val();
+      var password = $("#login-password").val();
+      var title = BASE_TITLE;
+
+      if (username) {
+        $("#login").fadeOut();
+        title = username + "'s " + BASE_TITLE;
+
+        $(".requires-login").show();
+        if (password) {
+          $(".requires-auth-login").show();
+        } else {
+          $(".requires-auth-login").hide();
+        }
+      } else {
+        $("#login").fadeIn();
+        $(".requires-login").hide();
+        $(".requires-auth-login").hide();
+      }
+
+      if (document.title != title) {
+        document.title = title;
+        $("#header .title").text(title);
+      }
+    }
+
+    $("#header .menu li").click(
       function(event) {
-        // TODO: Change section.
-      },
-      false
-    );
+        var dialog = $("#" + this.title);
+        if (dialog.length == 0)
+          throw new Error("dialog not found: " + this.title);
+        dialog.fadeIn();
+      });
+
+    $("#login-form").submit(
+      function(event) {
+        event.preventDefault();
+        onLoginChange();
+      });
+
+    onLoginChange();
   });
