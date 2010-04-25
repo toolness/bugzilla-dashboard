84078980908af8814a4e2d3b659aeab6e3e26266
Atul Varma <avarma@mozilla.com>
2010-04-25 09:10:46 -0700

    cache now uses localstorage

--- js/modules/cache.js
+++ js/modules/cache.js
@@ -1,8 +1,15 @@
-Require.modules["cache"] = function(exports) {
-  var cache = {};
+Require.modules["cache"] = function(exports, require) {
+  var window = require("window");
+  var cache;
+
+  if (window.localStorage["cache"]) {
+    cache = JSON.parse(window.localStorage["cache"]);
+  } else
+    cache = {};
 
   exports.set = function set(key, value) {
     cache[key] = value;
+    window.localStorage["cache"] = JSON.stringify(cache);
   };
   exports.get = function get(key) {
     return cache[key];
