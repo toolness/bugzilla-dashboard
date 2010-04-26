191f8c7a3d1c8e8acda95779316ee0575161af33
Atul Varma <avarma@mozilla.com>
2010-04-25 19:31:19 -0700

    Fixed cache.js for iPad.

--- js/modules/cache.js
+++ js/modules/cache.js
@@ -1,15 +1,23 @@
 Require.modules["cache"] = function(exports, require) {
   var window = require("window");
-  var cache;
+  var cache = window.localStorage.getItem("cache");
 
-  if (window.localStorage["cache"]) {
-    cache = JSON.parse(window.localStorage["cache"]);
-  } else
+  if (cache)
+    cache = JSON.parse(cache);
+  else
     cache = {};
 
   exports.set = function set(key, value) {
     cache[key] = value;
-    window.localStorage["cache"] = JSON.stringify(cache);
+
+    // Remove the key first, to get around a strange iPad
+    // issue: http://stackoverflow.com/questions/2603682/is-anyone-else-receiving-a-quota-exceeded-err-on-their-ipad-when-accessing-locals
+    window.localStorage.removeItem("cache");
+
+    // TODO: We should really catch QUOTA_EXCEEDED_ERR here,
+    // which could be thrown if the user is in private
+    // browsing mode.
+    window.localStorage.setItem("cache", JSON.stringify(cache));
   };
   exports.get = function get(key) {
     return cache[key];
